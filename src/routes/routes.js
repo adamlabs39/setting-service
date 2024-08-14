4d1ad250ba088d6e998beb78f03e12f499093782
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-08-14 17:12:58 +0700

    add base url in route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,27 +4,30 @@ import IntegrationController from "../controllers/integration-controller.js";
 import ProfileController from "../controllers/profile-controller.js";
 import PrinterController from "../controllers/printer-controller.js";
 
+const apiBase = process.env.API_BASE || "api";
+const apiVersion = process.env.API_VERSION || "v1";
+
 const routes = express.Router();
 // routes.use(authorizationMiddleware);
 
 // HEALTH CHECK
-routes.get("/health", (req, res) => res.status(200).json({ message: "OK" }));
+routes.get(`/${apiBase}/${apiVersion}/health`, (req, res) => res.status(200).json({ message: "OK" }));
 
 // FASKES PROFILE
-routes.get("/faskes-profile", FaskesProfileController.findByFaskesUuid);
-routes.put("/faskes-profile", FaskesProfileController.update);
+routes.get(`/${apiBase}/${apiVersion}/faskes-profile`, FaskesProfileController.findByFaskesUuid);
+routes.put(`/${apiBase}/${apiVersion}/faskes-profile`, FaskesProfileController.update);
 
 // INTERGRATION
-routes.get("/integration", IntegrationController.findByUuid);
-routes.put("/integration/vclaim", IntegrationController.updateVclaim);
-routes.put("/integration/other", IntegrationController.updateOther);
+routes.get(`/${apiBase}/${apiVersion}/integration`, IntegrationController.findByUuid);
+routes.put(`/${apiBase}/${apiVersion}/integration/vclaim`, IntegrationController.updateVclaim);
+routes.put(`/${apiBase}/${apiVersion}/integration/other`, IntegrationController.updateOther);
 
 // PROFILE
-routes.get("/profile", ProfileController.getProfile);
-routes.put("/profile", ProfileController.updateProfile);
+routes.get(`/${apiBase}/${apiVersion}/profile`, ProfileController.getProfile);
+routes.put(`/${apiBase}/${apiVersion}/profile`, ProfileController.updateProfile);
 
 // PRINTER
-routes.get("/printer", PrinterController.findByUuid);
-routes.put("/printer", PrinterController.update);
+routes.get(`/${apiBase}/${apiVersion}/printer`, PrinterController.findByUuid);
+routes.put(`/${apiBase}/${apiVersion}/printer`, PrinterController.update);
 
 export default routes;
