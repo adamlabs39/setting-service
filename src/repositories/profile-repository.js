ce9607c6e29fb2d50c40da32d7be9286c817b6e6
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-08-22 10:40:57 +0700

    add awalan_gelar & akhiran_gelar in getProfile

--- src/repositories/profile-repository.js
+++ src/repositories/profile-repository.js
@@ -20,7 +20,7 @@ export default class ProfileRepository {
                         }
                     ]
                 },
-                attributes: ["uuid", "username", "name", "email", 'password' , "photo", "role_uuid", 'phone', 'inventory_medis', 'inventory_non_medis'],
+                attributes: ["uuid", "username", "name", "email", 'password' , "photo", "role_uuid", 'phone', 'inventory_medis', 'inventory_non_medis', 'awalan_gelar', 'akhiran_gelar'],
                 transaction: tr
             });
 
