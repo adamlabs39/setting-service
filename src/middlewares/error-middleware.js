c4a5f3967c905da6eea27ef186cb2763cf89f6ef
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-11-07 13:26:18 +0700

    fix integration of authorization sdk

--- src/middlewares/error-middleware.js
+++ src/middlewares/error-middleware.js
@@ -2,6 +2,7 @@ import NotfoundException from "../errors/notfound-exception.js";
 import BadRequestException from "../errors/bad-request-exception.js";
 import errorResponse from "../responses/error-response.js";
 import InternalServerException from "../errors/internal-server-exception.js";
+import AuthorizationSdkException from "@adameds/authorization-sdk/sdkException";
 
 const errorMiddleware = (error, request, response, nextFunction) => {
     if (error instanceof NotfoundException) {
@@ -10,8 +11,9 @@ const errorMiddleware = (error, request, response, nextFunction) => {
         response.status(error.status).json(errorResponse(error.message));
     } else if (error instanceof InternalServerException) {
         response.status(error.code).json(errorResponse(error.message));
+    }else if (error instanceof AuthorizationSdkException) {
+        response.status(error.code).json(error.message);
     }
-
     response.status(500).json(errorResponse(error.message));
 };
 
